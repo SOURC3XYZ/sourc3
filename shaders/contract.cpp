c89d8f03436435613f84589929844f974916ec20
Ilya Kapitonau <ilya.th.kapitonov@gmail.com>
2021-11-11 16:38:41 +0300

    Rewrite contract.cpp with heap memory allocation

--- shaders/contract.cpp
+++ shaders/contract.cpp
@@ -3,9 +3,21 @@
 #include "Shaders/Math.h"
 #include "contract.h"
 
-#include <utility>
+#include <tuple>
 
 using namespace GitRemoteBeam;
+
+namespace {
+	enum Operations {
+		REPO_SIZE,
+		REPO,
+		OBJECTS_NUMBER,
+		OBJECTS,
+		REFS_NUMBER,
+		REFS,
+	};
+	constexpr Operations ALL_OPERATIONS[] = { REPO_SIZE, REPO, OBJECTS_NUMBER, OBJECTS, REFS_NUMBER, REFS };
+}
 
 BEAM_EXPORT void Ctor(InitialParams& params)
 {
@@ -19,74 +31,98 @@ BEAM_EXPORT void Dtor(void*)
 
 BEAM_EXPORT void Method_2(const CreateRepoParams& params)
 {
-	auto key = std::make_pair(params.repo.owner, params.repo.name);
-	RepoInfo repo_info;
+	auto key = std::make_tuple(params.repo_owner, params.repo_name, ::Operations::REPO_SIZE);
+	size_t repo_size;
 
 	// halt if repo with this owner exists
-	if (Env::LoadVar_T(key, repo_info))
+	if (Env::LoadVar_T(key, repo_size)) {
 		Env::Halt();
+	}
 
-	strncpy(repo_info.name, params.repo.name, MAX_NAME_SIZE);
-	repo_info.owner = params.repo.owner;
+	RepoInfo repo_info;
+
+	strncpy(repo_info.name, params.repo_name, MAX_NAME_SIZE);
+	repo_info.owner = params.repo_owner;
 	repo_info.users_number = 0;
-	Env::AddSig(params.repo.owner);
+
+	Env::SaveVar_T(key, sizeof(repo_info));
+	
+	key = std::make_tuple(repo_info.owner, repo_info.name, ::Operations::REPO);
 	Env::SaveVar_T(key, repo_info);
+
+	key = std::make_tuple(repo_info.owner, repo_info.name, ::Operations::OBJECTS_NUMBER);
+	Env::SaveVar_T(key, size_t(0));
+
+	key = std::make_tuple(repo_info.owner, repo_info.name, ::Operations::REFS_NUMBER);
+	Env::SaveVar_T(key, size_t(0));
+
+	Env::AddSig(repo_info.owner);
 }
 
 BEAM_EXPORT void Method_3(const DeleteRepoParams& params)
 {
-	auto key = std::make_pair(params.repo.owner, params.repo.name);
-	RepoInfo repo_info;
-	if (Env::LoadVar_T(key, repo_info)) {
-		Env::DelVar_T(key);
+	auto key = std::make_tuple(params.repo_owner, params.repo_name, ::Operations::REPO_SIZE);
+	size_t repo_size;
+	
+	if (Env::LoadVar_T(key, repo_size)) {
+		for (auto op : ::ALL_OPERATIONS) {
+			auto key = std::make_tuple(params.repo_owner, params.repo_name, op);
+			Env::DelVar_T(key);
+		}
 	} else {
 		Env::Halt();
 	}
-	Env::AddSig(params.repo.owner);
+	Env::AddSig(params.repo_owner);
 }
 
-BEAM_EXPORT void Method_4(AddUserParams& params)
+BEAM_EXPORT void Method_4(const AddUserParams& params)
 {
-	auto key = std::make_pair(params.repo.owner, params.repo.name);
-	RepoInfo repo_info;
+	auto key = std::make_tuple(params.repo_owner, params.repo_name, ::Operations::REPO_SIZE);
+	size_t repo_size;
 
-	if (!Env::LoadVar_T(key, repo_info)) {
+	if (!Env::LoadVar_T(key, repo_size)) {
 		Env::Halt();
 	} else {
-		if (params.repo.users_number == repo_info.users_number + 1) {
-			params.repo.users[0] = params.user;
-			for (size_t i = 1; i < params.repo.users_number; ++i) {
-				params.repo.users[i] = repo_info.users[i - 1];
-			}
-			Env::SaveVar_T(key, params.repo);
-		} else {
-			Env::Halt();
-		}
+		Env::SaveVar_T(key, repo_size + sizeof(PubKey));
+
+		key = std::make_tuple(params.repo_owner, params.repo_name, ::Operations::REPO);
+		RepoInfo* repo_info = (RepoInfo*)Env::Heap_Alloc(repo_size + sizeof(PubKey));
+		Env::LoadVar(&key, sizeof(key), repo_info, repo_size, KeyTag::Internal);
+
+		repo_info->users[repo_info->users_number++] = params.user;
+		Env::SaveVar(&key, sizeof(key), repo_info, repo_size + sizeof(PubKey), KeyTag::Internal);
+		Env::Heap_Free(repo_info);
 	}
 
-	Env::AddSig(params.repo.owner);
+	Env::AddSig(params.repo_owner);
 }
 
-BEAM_EXPORT void Method_5(RemoveUserParams& params)
+BEAM_EXPORT void Method_5(const RemoveUserParams& params)
 {
-	auto key = std::make_pair(params.repo.owner, params.repo.name);
-	RepoInfo repo_info;
+	auto key = std::make_tuple(params.repo_owner, params.repo_name, ::Operations::REPO_SIZE);
+	size_t repo_size;
 
-	if (!Env::LoadVar_T(key, repo_info)) {
+	if (!Env::LoadVar_T(key, repo_size)) {
 		Env::Halt();
 	} else {
-		if (params.repo.users_number > 0 && params.repo.users_number + 1 == repo_info.users_number) {
-			size_t last_idx = 0;
-			PubKey deleted_user = params.user;
-			for (size_t i = 0; i < repo_info.users_number; ++i) {
-				if (Env::Memcmp(&deleted_user, &repo_info.users[i], sizeof(deleted_user)))
-					params.repo.users[last_idx++] = repo_info.users[i];
+		key = std::make_tuple(params.repo_owner, params.repo_name, ::Operations::REPO);
+		RepoInfo* repo_info = (RepoInfo*)Env::Heap_Alloc(repo_size);
+		Env::LoadVar(&key, sizeof(key), repo_info, repo_size, KeyTag::Internal);
+
+		size_t deleted_users_number = 0;
+		for (size_t i = 0; i < repo_info->users_number; ++i) {
+			if (Env::Memcmp(&params.user, &repo_info->users[i], sizeof(params.user))) {
+				++deleted_users_number;
+			} else if (deleted_users_number > 0) {
+				repo_info->users[i - deleted_users_number] = repo_info->users[i];
 			}
-			Env::SaveVar_T(key, params.repo);
-		} else {
-			Env::Halt();
 		}
+
+		Env::SaveVar(&key, sizeof(key), repo_info, repo_size - deleted_users_number * sizeof(PubKey), KeyTag::Internal);
+		key = std::make_tuple(params.repo_owner, params.repo_name, ::Operations::REPO_SIZE);
+		Env::SaveVar_T(key, repo_size - deleted_users_number * sizeof(PubKey));
+		Env::Heap_Free(repo_info);
 	}
 
-	Env::AddSig(params.repo.owner);
+	Env::AddSig(params.repo_owner);
 }
