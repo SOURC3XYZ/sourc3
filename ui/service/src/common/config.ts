935054a241edf2729209fc4261e208520eb2684c
SixStringer91 <danlootsev@gmail.com>
2022-02-03 19:31:04 +0300

    ui/service: add rootpath flag

--- ui/service/src/common/config.ts
+++ ui/service/src/common/config.ts
@@ -1,10 +1,21 @@
 import dotenv from 'dotenv';
 import path from 'path';
+import { program } from 'commander';
 
-export const rootPath = path.join(__dirname, '../../');
+const buildConfig = () => {
+  program
+    .option('-l, --user_data_loc <string>', 'set root path', './');
+
+  program.parse(process.argv);
+  const options = program.opts();
+  const { user_data_loc } = options;
+  return user_data_loc;
+};
+
+export const rootPath = buildConfig();
 
 dotenv.config({
-  path: path.join(rootPath, '.env')
+  path: path.join(path.join(__dirname, '../../'), '.env')
 });
 
 export const {
