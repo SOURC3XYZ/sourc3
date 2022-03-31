504dc425f8c3500f8e540411f6a46a0562cabd35
Nikita Shlyago <85184722+nikkitashl@users.noreply.github.com>
2022-03-31 14:14:03 +0300

    First step on creating custom action for downloads

---
+++ .github/actions/download-beam-assets/main.js
@@ -0,0 +1,185 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const AdmZip = require('adm-zip')
+const filesize = require('filesize')
+const pathname = require('path')
+const fs = require('fs')
+
+function checkArtifact(artifact_name, platform_name, network_name, downloadable_artifacts) {
+    var is_target_artifact = false;
+    for (const target_artifact of downloadable_artifacts) {
+        if (artifact.name.indexOf(platform_name) != -1) {
+            is_target_artifact = true;
+            break;
+        }
+    }
+    if (!is_target_artifact) {
+        return false;
+    }
+
+    return artifact.name.indexOf(platform_name) != -1 && 
+            (artifact.name.indexOf(network_name) != -1 || artifact.name.indexOf("wasm") != -1)
+}
+
+async function main() {
+    const downloadable_artifacts = ["cli", "api", "node", "wasm"];
+
+    try {
+        const token = core.getInput("github_token", { required: true })
+        const workflow = core.getInput("workflow", { required: true })
+        const [owner, repo] = core.getInput("repo", { required: true }).split("/")
+        const path = core.getInput("path", { required: true })
+        const platform_name = core.getInput("platform_name")
+        const network_name = core.getInput("net_name")
+        let workflowConclusion = core.getInput("workflow_conclusion")
+        let pr = core.getInput("pr")
+        let commit = core.getInput("commit")
+        let branch = core.getInput("branch")
+        let event = core.getInput("event")
+        let runID = core.getInput("run_id")
+        let runNumber = core.getInput("run_number")
+        let checkArtifacts = core.getInput("check_artifacts")
+        let searchArtifacts = core.getInput("search_artifacts")
+
+        const client = github.getOctokit(token)
+
+        console.log("==> Workflow:", workflow)
+
+        console.log("==> Repo:", owner + "/" + repo)
+
+        console.log("==> Conclusion:", workflowConclusion)
+
+        if (pr) {
+            console.log("==> PR:", pr)
+
+            const pull = await client.pulls.get({
+                owner: owner,
+                repo: repo,
+                pull_number: pr,
+            })
+            commit = pull.data.head.sha
+            branch = pull.data.head.ref
+        }
+
+        if (commit) {
+            console.log("==> Commit:", commit)
+        }
+
+        if (branch) {
+            branch = branch.replace(/^refs\/heads\//, "")
+            console.log("==> Branch:", branch)
+        }
+
+        if (event) {
+            console.log("==> Event:", event)
+        }
+
+        if (runNumber) {
+            console.log("==> RunNumber:", runNumber)
+        }
+
+        if (!runID) {
+            for await (const runs of client.paginate.iterator(client.actions.listWorkflowRuns, {
+                owner: owner,
+                repo: repo,
+                workflow_id: workflow,
+                branch: branch,
+                event: event,
+            }
+            )) {
+                for (const run of runs.data) {
+                    if (commit && run.head_sha != commit) {
+                        continue
+                    }
+                    if (runNumber && run.run_number != runNumber) {
+                        continue
+                    }
+                    if (workflowConclusion && (workflowConclusion != run.conclusion && workflowConclusion != run.status)) {
+                        continue
+                    }
+                    if (checkArtifacts || searchArtifacts) {
+                        let artifacts = await client.actions.listWorkflowRunArtifacts({
+                            owner: owner,
+                            repo: repo,
+                            run_id: run.id,
+                        })
+                        if (artifacts.data.artifacts.length == 0) {
+                            continue
+                        }
+                        if (searchArtifacts) {
+                            const artifact = artifacts.data.artifacts.find((artifact) => {
+                                return checkArtifact(artifact, platform_name, network_name, downloadable_artifacts);
+                            })
+                            if (!artifact) {
+                                continue
+                            }
+                        }
+                    }
+                    runID = run.id
+                    break
+                }
+                if (runID) {
+                    break
+                }
+            }
+        }
+
+        if (runID) {
+            console.log("==> RunID:", runID)
+        } else {
+            throw new Error("no matching workflow run found")
+        }
+
+        let artifacts = await client.paginate(client.actions.listWorkflowRunArtifacts, {
+            owner: owner,
+            repo: repo,
+            run_id: runID,
+        })
+
+        // One artifact or all if `name` input is not specified.
+        if (name) {
+            artifacts = artifacts.filter((artifact) => {
+                return checkArtifact(artifact, platform_name, network_name, downloadable_artifacts);
+            })
+        }
+
+        if (artifacts.length == 0)
+            throw new Error("no artifacts found")
+
+        for (const artifact of artifacts) {
+            console.log("==> Artifact:", artifact.id)
+
+            const size = filesize(artifact.size_in_bytes, { base: 10 })
+
+            console.log(`==> Downloading: ${artifact.name}.zip (${size})`)
+
+            const zip = await client.actions.downloadArtifact({
+                owner: owner,
+                repo: repo,
+                artifact_id: artifact.id,
+                archive_format: "zip",
+            })
+
+            const dir = name ? path : pathname.join(path, artifact.name)
+
+            fs.mkdirSync(dir, { recursive: true })
+
+            const adm = new AdmZip(Buffer.from(zip.data))
+
+            adm.getEntries().forEach((entry) => {
+                const action = entry.isDirectory ? "creating" : "inflating"
+                const filepath = pathname.join(dir, entry.entryName)
+
+                console.log(`  ${action}: ${filepath}`)
+            })
+
+            adm.extractAllTo(dir, true)
+        }
+    } catch (error) {
+        core.setOutput("error_message", error.message)
+        core.setFailed(error.message)
+    }
+}
+
+main()
+
