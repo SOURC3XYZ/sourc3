fdd54ecdf7348f4123dc024536618a5785e79873
cat <user@users-MacBook-Pro.local>
2022-05-17 14:29:45 +0300

    adaptive

---
+++ ui/front/src/components/shared/repo/content/repo-content/content/upper-menu/content/repo-description/repo-description.tsx
@@ -0,0 +1,41 @@
+import { Info } from '@components/shared';
+import { BranchCommit } from '@types';
+import { Col } from 'antd';
+import styles from './repo-meta.module.scss';
+
+type RepoMetaProps = {
+  commit: BranchCommit;
+};
+
+function RepoMeta({ commit }: RepoMetaProps) {
+  const data = [
+    {
+      title: 'Author: ',
+      message: commit.author_name,
+      link: commit.author_email
+    },
+    {
+      title: 'Last comitter: ',
+      message: commit.committer_name,
+      link: commit.committer_email
+    },
+    {
+      title: 'Commit: ',
+      message: commit.raw_message,
+      link: commit.raw_header
+    }
+  ];
+
+  const dataRender = data.map(({ title, message, link }, i) => (
+    <Col key={`repo-meta-${title}`} offset={i && 1}>
+      <Info title={title} message={message} link={link} />
+    </Col>
+  ));
+  return (
+    <div className={styles.meta}>
+      {dataRender}
+    </div>
+  );
+}
+
+export default RepoMeta;
