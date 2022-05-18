eef5b928a19d247f22da905c73d7240eb94bdba0
SixStringer91 <danlootsev@gmail.com>
2022-05-18 17:27:18 +0300

    update wasm, cid

--- ui/front/src/libs/redux/reducers/entities.reducer.ts
+++ ui/front/src/libs/redux/reducers/entities.reducer.ts
@@ -23,8 +23,8 @@ const reducer = (
   const newState = JSON.parse(JSON.stringify(state)) as IRepos;
   switch (action.type) {
     case ACTIONS.GET_ALL_REPOS: {
-      newState.repos = [...(action.payload as IRepos['repos'])]
-        .filter((el) => !String(el.repo_id).match(/^(1|3|4|5|6|7|8)$/));
+      newState.repos = [...(action.payload as IRepos['repos'])];
+      // .filter((el) => !String(el.repo_id).match(/^(1|3|4|5|6|7|8)$/));
       return newState;
     }
     case ACTIONS.SET_SEARCH: {
@@ -32,14 +32,14 @@ const reducer = (
       return newState;
     }
     case ACTIONS.SET_ORGANIZATIONS: {
-      newState.organizations = (action.payload as IRepos['organizations'])
-        .filter((el) => !String(el.organization_id).match(/^(1|3)$/));
+      newState.organizations = (action.payload as IRepos['organizations']);
+      // .filter((el) => !String(el.organization_id).match(/^(1|3)$/));
 
       return newState;
     }
     case ACTIONS.SET_PROJECTS: {
-      newState.projects = (action.payload as IRepos['projects'])
-        .filter((el) => !String(el.organization_id).match(/^(1|3)$/));
+      newState.projects = (action.payload as IRepos['projects']);
+      // .filter((el) => !String(el.organization_id).match(/^(1|3)$/));
       return newState;
     }
     default:
