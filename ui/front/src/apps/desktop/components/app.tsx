a324f45acf83c2246d6d48a75ee152894168c425
JenkJS <jenkjobs@gmail.com>
2022-06-08 22:18:43 +0300

    fix navigate

--- ui/front/src/apps/desktop/components/app.tsx
+++ ui/front/src/apps/desktop/components/app.tsx
@@ -25,7 +25,7 @@ function App() {
   const routesData = [
     {
       path: '/',
-      element: <Navigate replace to="auth/restore" />
+      element: <Navigate replace to="auth/" />
     },
     {
       path: 'auth/*',
