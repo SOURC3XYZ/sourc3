6f4bf16a40f2c0cf1135d365990ac4b14471daf8
JenkJS <jenkjobs@gmail.com>
2022-07-26 11:40:47 +0300

    add: clone button on repos page

--- ui/front/src/components/shared/repo/repo.container.tsx
+++ ui/front/src/components/shared/repo/repo.container.tsx
@@ -1,7 +1,11 @@
 import { ErrorBoundary, PreloadComponent } from '@components/hoc';
-import { BackButton, FailPage, Preload } from '@components/shared';
+import {
+  BackButton, FailPage, Preload, NavButton
+} from '@components/shared';
 import { LoadingMessages } from '@libs/constants';
+import { useAllRepos } from '@libs/hooks/container/all-repos';
 import { useUserRepos } from '@libs/hooks/container/user-repos';
+import { message } from 'antd';
 import Title from 'antd/lib/typography/Title';
 import { useCallback, useMemo } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
@@ -13,10 +17,23 @@ import styles from './repo.module.scss';
 
 function UserRepos() {
   const containerProps = useUserRepos();
+  const allReposProps = useAllRepos();
+
+  const { items } = allReposProps;
 
   const {
-    isLoaded, repoName, commitsMap, loadingHandler, startLoading
+    isLoaded, repoName, commitsMap, loadingHandler, startLoading, id
   } = containerProps;
+
+  const item = items && items.filter((el) => el.repo_id === id);
+  const { repo_owner } = item[0];
+
+  const repoLink = `sourc3://${repo_owner}/${repoName}`;
+
+  const handleCloneRepo = () => {
+    navigator.clipboard.writeText(repoLink);
+    message.info(`${repoLink} copied to clipboard!`);
+  };
 
   const navigate = useNavigate();
 
@@ -46,9 +63,14 @@ function UserRepos() {
   return (
     <div className={styles.wrapper}>
       {isElectron ? <BackButton onClick={back} /> : null}
-
       <div className={styles.titleWrapper}>
         <Title className={styles.title} level={3}>{repoName}</Title>
+        <NavButton
+          name="Clone"
+          onClick={handleCloneRepo}
+          inlineStyles={{ margin: '0.5rem', width: '60px', padding: '5px' }}
+          active
+        />
         <ReloadBtn isLoaded={isLoadedReload} loadingHandler={startLoading} />
       </div>
 
