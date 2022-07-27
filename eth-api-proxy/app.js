d4610fc1612a85690c794504881a92e21e62f4cd
roman-strilets <roman@beam-mw.com>
2022-07-27 17:04:28 +0300

    use ethers instead of web3js

--- eth-api-proxy/app.js
+++ eth-api-proxy/app.js
@@ -1,38 +1,28 @@
 const jayson = require('jayson/promise');
 const dotenv = require("dotenv");
-const Web3 = require("web3");
-const Sourc3Contract = require("./utils/Sourc3.json");
-const eth_utils = require("./utils/eth_utils.js");
+const ethers = require('ethers');
 
 dotenv.config()
 
-const web3ProviderOptions = {
-    // Enable auto reconnection
-    reconnect: {
-        auto: true,
-        delay: 5000, // ms
-        maxAttempts: 5,
-        onTimeout: false,
-    },
-}
-let web3 = new Web3(
-    new Web3.providers.HttpProvider(
-        process.env.ETH_HTTP_PROVIDER,
-        web3ProviderOptions
-    )
-)
-const sourc3Contract = new web3.eth.Contract(
-    Sourc3Contract.abi,
-    process.env.ETH_SOURC3_CONTRACT
-);
+const provider = new ethers.providers.JsonRpcProvider(process.env.ETH_HTTP_PROVIDER);
+const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC, process.env.HD_PATH);
+const walletSigner = wallet.connect(provider);
+const abi = [
+    "function getRepo(uint64 id) public view returns (string memory, address, uint64, uint64, uint64, string memory)",
+    "function loadState(uint64 repoId) public view returns (string memory state, uint64 curObjects, uint64 curMetas)",
+    "function getRepoId(address owner, string memory name) public view returns (uint64)",
+    "function pushState(uint64 repoId, uint64 objsCount, uint64 metasCount, string memory expectedState, string memory state) public"
+];
+const sourc3Contract = new ethers.Contract(process.env.ETH_SOURC3_CONTRACT, abi, walletSigner);
 
 (async () => {
     // create a server
     const server = new jayson.Server({
         get_netid: async function() {
             try {
-                return await web3.eth.net.getId()
-                // return await web3.eth.net.getNetworkType() // This is not a 100% accurate
+                let network = await provider.getNetwork();
+
+                return network.chainId;
             } catch (err) {
                 logger.error(`net.getId is failed`)
                 throw err
@@ -40,7 +30,8 @@ const sourc3Contract = new web3.eth.Contract(
         },
         getRepoId: async function(args) {
             try {
-                return await sourc3Contract.methods.getRepoId(args.owner, args.name).call()
+                let repoId = await sourc3Contract.getRepoId(args.owner, args.name);
+                return repoId.toString();
             } catch (err) {
                 console.error(`getRepoId is failed - `, err)
                 throw err
@@ -48,7 +39,7 @@ const sourc3Contract = new web3.eth.Contract(
         },
         getRepo: async function(args) {
             try {
-                return await sourc3Contract.methods.getRepo(args.repoId).call()
+                return await sourc3Contract.getRepo(args.repoId);
             } catch (err) {
                 console.error(`getRepo is failed - `, err)
                 throw err
@@ -56,7 +47,13 @@ const sourc3Contract = new web3.eth.Contract(
         },
         loadState: async function(args) {
             try {
-                return await sourc3Contract.methods.loadState(args.repoId).call()
+                let result = await sourc3Contract.loadState(args.repoId);
+                let state = {
+                    "state": result[0],
+                    "curObjects": result[1].toString(),
+                    "curMetas": result[2].toString()
+                }
+                return state;
             } catch (err) {
                 console.error(`loadState is failed - `, err)
                 throw err
@@ -64,16 +61,10 @@ const sourc3Contract = new web3.eth.Contract(
         },
         pushState: async function(args) {
             try {
-                const tx = sourc3Contract.methods.pushState(args.repoId, args.objsCount,
-                     args.metasCount, args.expectedState, args.state)
+                let tx = await sourc3Contract.pushState(args.repoId, args.objsCount,
+                    args.metasCount, args.expectedState, args.state)
 
-                const txReceipt = await eth_utils.requestToContract(
-                    web3,
-                    process.env.ETH_ADDRESS,
-                    process.env.ETH_SOURC3_CONTRACT,
-                    process.env.ETH_PRIVATE_KEY,
-                    tx.encodeABI(),
-                    process.env.ETH_TEST_GAS_LIMIT);
+                let txReceipt = await tx.wait();
                 
                 if (txReceipt.status) {
                     return {"transactionHash": txReceipt["transactionHash"]};
--- eth-api-proxy/tools/create_organization.js
+++ eth-api-proxy/tools/create_organization.js
@@ -1,31 +1,16 @@
 const dotenv = require("dotenv");
+const program = require("commander");
+const ethers = require('ethers');
 
 dotenv.config();
 
-const eth_utils = require("../utils/eth_utils.js");
-const Web3 = require("web3");
-const Sourc3Contract = require("../utils/Sourc3.json");
-const program = require("commander");
-
-let web3_instance = new Web3(new Web3.providers.HttpProvider(process.env.ETH_HTTP_PROVIDER));
-const sourc3Contract = new web3_instance.eth.Contract(
-    Sourc3Contract.abi,
-    process.env.ETH_SOURC3_CONTRACT
-);
-
-let createOrganization = async (name) => {
-    const tx = sourc3Contract.methods.createOrganization(name);
-
-    const txReceipt = await eth_utils.requestToContract(
-        web3_instance,
-        process.env.ETH_ADDRESS,
-        process.env.ETH_SOURC3_CONTRACT,
-        process.env.ETH_PRIVATE_KEY,
-        tx.encodeABI(),
-        process.env.ETH_TEST_GAS_LIMIT);
-
-    return txReceipt;
-}
+const provider = new ethers.providers.JsonRpcProvider(process.env.ETH_HTTP_PROVIDER);
+const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC, process.env.HD_PATH);
+const walletSigner = wallet.connect(provider);
+const abi = [
+    "function createOrganization(string memory name) public"
+];
+const sourc3Contract = new ethers.Contract(process.env.ETH_SOURC3_CONTRACT, abi, walletSigner);
 
 (async () => {
     console.log("Calling 'createOrganization' of Sourc3 contract:")
@@ -34,8 +19,10 @@ let createOrganization = async (name) => {
     program.parse(process.argv)
 
     const options = program.opts()
-    const txReceipt = await createOrganization(options.name)
 
+    let tx = await sourc3Contract.createOrganization(options.name);
+    let txReceipt = await tx.wait();
+    
     console.log("txReceipt: ", txReceipt)
     console.log("'createOrganization' is finished.")
 })();
