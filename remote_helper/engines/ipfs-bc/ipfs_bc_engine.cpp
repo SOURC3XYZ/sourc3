bac063154528df7b9a1ac8ace5297d464217e7cb
Nikita Shlyago <85184722+nikkitashl@users.noreply.github.com>
2022-10-06 15:47:18 +0300

    Add missing licence

--- remote_helper/engines/ipfs-bc/ipfs_bc_engine.cpp
+++ remote_helper/engines/ipfs-bc/ipfs_bc_engine.cpp
@@ -1,3 +1,17 @@
+// Copyright 2021-2022 SOURC3 Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "ipfs_bc_engine.h"
 
 #include <iostream>
@@ -218,7 +232,7 @@ IEngine::CommandResult IPFSBlockChainEngine::DoFetchAsync(const vector<string_vi
             auto it_to_receive = object_hashes.begin();
             const auto& object_to_receive = *it_to_receive;
             auto pit = processing_hashes.insert(object_to_receive);
-            if (pit.second == false) {
+            if (!pit.second) {
                 object_hashes.erase(it_to_receive);
                 continue;
             }
@@ -437,7 +451,7 @@ IEngine::CommandResult IPFSBlockChainEngine::DoPush(const vector<string_view>& a
 
             bool last = (done == objs.size());
             client_.PushObjects(str_data, collector.m_refs, last);
-            return last == false;  // continue
+            return !last;  // continue
         });
     }
     {
