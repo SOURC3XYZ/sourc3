6a1b12a1cb8d8535b91f0df37f45cd0c9abd8f16
SixStringer91 <danlootsev@gmail.com>
2022-10-06 18:19:56 +0300

    git user menu

--- ui/front/src/components/shared/profile-block/profile-block.tsx
+++ ui/front/src/components/shared/profile-block/profile-block.tsx
@@ -1,7 +1,9 @@
 import { AddButtonOrg, Balance, Profile } from '@components/shared';
 import Avatar from '@components/shared/git-auth/profile/avatar/avatar';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from '@libs/redux';
+import { Button, Dropdown, Menu } from 'antd';
+import { useGitProfile } from '@libs/hooks/thunk';
 import styles from './profile-block.module.scss';
 
 type HeaderPropsType = {
@@ -16,15 +18,75 @@ function ProfileBlock({
 }:HeaderPropsType) {
   const login = useSelector((state) => state.profile.data.github_login);
 
+  const navigate = useNavigate();
+
+  const { setGitUserLogout } = useGitProfile();
+
+  const data = [
+    { title: 'Profile' },
+    { title: 'My repositories', isDisabled: true },
+    { title: 'My organization', isDisabled: true, delimeter: true },
+    {
+      title: 'Onboarding',
+      onClick: (e:any) => {
+        e.preventDefault();
+        navigate('/onboarding');
+      }
+    },
+    {
+      title: 'Referral program',
+      delimeter: true,
+      onClick: (e:any) => {
+        e.preventDefault();
+        navigate('/referral-programm');
+      }
+    },
+    { title: 'Twitter', link: 'https://twitter.com/SOURC3xyz' },
+    { title: 'Discord', link: 'https://discord.gg/nqTTMXrhMc', delimeter: true },
+    { title: 'Logout', onClick: setGitUserLogout }
+  ];
+  const menu = (
+    <Menu>
+      {data.map(({
+        title, onClick, link, isDisabled, delimeter
+      }) => (
+        <Menu.Item
+          className={delimeter ? styles.bordered : ''}
+          key={`menu-item-${title}`}
+        >
+          <Button
+            type="link"
+            className={styles.button}
+            href={link}
+            onClick={onClick}
+            disabled={isDisabled}
+            target="_blank"
+          >
+            {title}
+
+          </Button>
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+
   return (
     <div className={styles.wrapper}>
       {pKey && <AddButtonOrg />}
       {balance && <Balance />}
-      {profile && <Profile pKey={pKey} />}
+      {profile && pKey && <Profile pKey={pKey} />}
       {git && (
-        <Link to={`/profile/${login}`}>
-          <Avatar small />
-        </Link>
+        <Dropdown
+          overlay={menu}
+          placement="bottomCenter"
+          trigger={['hover']}
+          overlayClassName={styles.dropdown}
+          overlayStyle={{ position: 'fixed' }}
+        >
+          <Link to={`/profile/${login}`}>
+            <Avatar small />
+          </Link>
+        </Dropdown>
       ) }
     </div>
   );
